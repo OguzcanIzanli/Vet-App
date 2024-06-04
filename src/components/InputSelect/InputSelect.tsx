3f0bd845e2b75d2404b945f68f1c15c9563d286a
oguzcan izanli <oguzcanizanli@gmail.com>
2024-06-04 22:14:10 +0300

    InputSelect component fixed.

--- src/components/InputSelect/InputSelect.tsx
+++ src/components/InputSelect/InputSelect.tsx
@@ -1,31 +1,31 @@
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import { InputSelectType, SelectionType } from "./types";
+// import Box from "@mui/material/Box";
+// import InputLabel from "@mui/material/InputLabel";
+// import MenuItem from "@mui/material/MenuItem";
+// import FormControl from "@mui/material/FormControl";
+// import Select from "@mui/material/Select";
+// import { InputSelectType, SelectionType } from "./types";
 
-const InputSelect = ({ label, name, onChange, value }: InputSelectType) => {
-  return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={value}
-          label={label}
-          onChange={onChange}
-        >
-          {name?.map((item: SelectionType) => (
-            <MenuItem key={item.id} value={item}>
-              {item.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </Box>
-  );
-};
+// const InputSelect = ({ label, name, onChange, value }: InputSelectType) => {
+//   return (
+//     <Box sx={{ minWidth: 120 }}>
+//       <FormControl fullWidth>
+//         <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+//         <Select
+//           labelId="demo-simple-select-label"
+//           id="demo-simple-select"
+//           value={value}
+//           label={label}
+//           onChange={onChange}
+//         >
+//           {name?.map((item: SelectionType) => (
+//             <MenuItem key={item.id} value={item}>
+//               {item.name}
+//             </MenuItem>
+//           ))}
+//         </Select>
+//       </FormControl>
+//     </Box>
+//   );
+// };
 
-export default InputSelect;
+// export default InputSelect;
